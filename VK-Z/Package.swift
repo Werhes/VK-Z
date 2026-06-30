ebee2ef495613b1e0bc4329f095af2e2e9e367f6
Lyosha Shapchits <a65768795@gmail.com>
2026-06-30 21:48:52 +0300

    new fix oof

--- VK-Z/Package.swift
+++ VK-Z/Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "VKZ",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v17),
+        .macOS(.v11)
     ],
     products: [
         .library(
